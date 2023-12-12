a0b84d058d971a07c751fe80ea42fc7c6ba6e2e5
Beomseok <jakebox1966@gmail.com>
2023-12-12 16:38:10 +0900

    배포오류 수정

---
+++ src/app/[locale]/common/layouts/header/Navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { Button } from '@material-tailwind/react'
+import { useTranslations } from 'next-intl'
+import { locales } from '@/i18nconfig'
+import { createSharedPathnamesNavigation } from 'next-intl/navigation'
+import * as React from 'react'
+
+export interface INavbarProps {}
+
+export default function Navbar(props: INavbarProps) {
+    const t = useTranslations('Layout.header.nav_bar')
+    const { Link } = createSharedPathnamesNavigation({ locales })
+    const menuList = ['about', 'gallery', 'crew', 'buy', 'sns', 'utility', 'admin']
+    return (
+        <div className="relative flex items-center gap-12 pr-4 font-bold">
+            {menuList.map((menu) => (
+                <Link key={menu} href={`/${menu}`} className="cursor-pointer hover:opacity-40">
+                    {t(menu)}
+                </Link>
+            ))}
+        </div>
+    )
+}
