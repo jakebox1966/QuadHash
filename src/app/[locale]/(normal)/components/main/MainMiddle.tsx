92241b27d66d2db2620fd4a560c52cd2d6b9d4ed
jake <jakebox1966@gmail.com>
2023-12-25 22:39:29 +0900

    #Quadhash - 메인 퍼블리싱

---
+++ src/app/[locale]/(normal)/components/main/MainMiddle.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export interface IMainMiddleProps {}
+
+export default function MainMiddle(props: IMainMiddleProps) {
+    return <></>
+}
---
+++ src/app/[locale]/(normal)/components/main/MainTop.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+
+export interface IMainTopProps {}
+
+export default function MainTop(props: IMainTopProps) {
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center bg-[#FFCD19] pt-40">
+                {/* <MainGridItems /> */}
+                <div className="max-w-[1600px]">
+                    <div className="text-white flex flex-col justify-center items-end">
+                        <div className="flex flex-col justify-end items-end px-10 text-7xl font-black pb-10">
+                            <div>QUADHASH</div>
+                            <div>HOME PAGE RENEW</div>
+                            <div className="text-sm font-light">
+                                QUADHASH 20,000 UNIQUE COLLECTIBLE.
+                            </div>
+                            <div className="text-[#F46221] font-semibold text-2xl py-4 pl-7 pr-4 mt-6 bg-white rounded-r-full rounded-tl-full">
+                                Explore on Opensea
+                            </div>
+                        </div>
+                        <div>
+                            <img src="/main_image1.svg" alt="test" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
