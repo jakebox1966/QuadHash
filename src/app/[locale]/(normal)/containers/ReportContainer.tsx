9f7e86f7a4df7519ae35070dae944efe69df290f
jake <jakebox1966@gmail.com>
2024-01-19 01:02:22 +0900

    #Quadhash - 신고센터 퍼블리싱

--- src/app/[locale]/(normal)/containers/ReportContainer.tsx
+++ src/app/[locale]/(normal)/containers/ReportContainer.tsx
@@ -7,7 +7,7 @@ import { useMetaMask } from '@/app/hooks/useMetaMask'
 import { getBlock, getTransfersForOwner } from '@/app/api/alchemy/api'
 import { Alchemy, GetTransfersForOwnerTransferType, Network, NftOrdering } from 'alchemy-sdk'
 import React from 'react'
-import { Button, Input, Textarea } from '@material-tailwind/react'
+import { Button, Checkbox, Input, Textarea } from '@material-tailwind/react'
 import { postReport } from '@/app/api/report/api'
 import { AlertContext } from '@/app/provider/AlertProvider'
 import { ConfirmContext } from '@/app/provider/ConfirmProvider'
@@ -238,9 +238,107 @@ export default function ReportContainer(props: IReportContainerProps) {
 
     return (
         <>
-            <div className="flex flex-col justify-start items-start h-full px-20">
-                <div>해킹 신고 센터</div>
-                <div className=" flex flex-row justify-start items-start w-full h-full">
+            <div className="flex flex-col justify-start items-start h-full px-20 min-h-[calc(100vh-176px)] gap-20">
+                <div>
+                    <div>QUADHASH</div>
+                    <div>
+                        해킹 신고 센터(우철이와 함께하는 광화문 해킹 투어) 1차 돈돈정 방문 2차 흡연
+                        구역(필수) 3차 닭발과 한잔
+                    </div>
+                </div>
+                <div className="flex flex-col gap-3">
+                    <div>주의사항 및 신고 안내 가이드</div>
+                    <div>
+                        <Checkbox
+                            label="해킹 피해 신고를 한 계정에 등록된 개인정보가 허위이거나 부정확하게 기입되어
+                        발생하는 모든 문제는 사용자 본인의 책임으로 간주하며, 이에 대해 동의합니다."
+                            ripple={false}
+                            // name="allAgreed"
+                            className="h-8 w-8 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                            crossOrigin={undefined}
+                            // checked={allAgreed}
+                            // onChange={handleAllAgreementChange}
+                        />
+                    </div>
+                    <div>
+                        <Checkbox
+                            label="신고된 계정에서 일어난 피해가 본인의 개인정보 유출이나 개인정보 관리 부실
+                        등으로 인해 발생하지 않았음을 확인합니다"
+                            ripple={false}
+                            // name="allAgreed"
+                            className="h-8 w-8 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                            crossOrigin={undefined}
+                            // checked={allAgreed}
+                            // onChange={handleAllAgreementChange}
+                        />
+                    </div>
+                    <div>
+                        <Checkbox
+                            label="사용자는 토큰 구매와 서비스 이용에 필요한 모든 조건을 충분히 이해하고, 이에
+                        동의합니다."
+                            ripple={false}
+                            // name="allAgreed"
+                            className="h-8 w-8 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                            crossOrigin={undefined}
+                            // checked={allAgreed}
+                            // onChange={handleAllAgreementChange}
+                        />
+                    </div>
+                    <div>
+                        <Checkbox
+                            label="해킹 피해 신고 시 제공한 모든 정보는 사실임을 확신하며, 허위로 기재하거나
+                        고의적으로 신고 내용에서 생략한 사실로 인해 발생하는 모든 문제와 책임은
+                        본인이 지게 됨을 약속합니다. (이 내용 및 신고 내용은 사법기관[사이버 수사대
+                        등]의 공식 요청이 있을 경우 제공될 수 있습니다.)"
+                            ripple={false}
+                            // name="allAgreed"
+                            className="h-8 w-8 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                            crossOrigin={undefined}
+                            // checked={allAgreed}
+                            // onChange={handleAllAgreementChange}
+                        />
+                    </div>
+                    <div>
+                        <Checkbox
+                            label="해킹 신고에 대해 정상적으로 신고 접수가 완료된 계정은 조사 및 복구를 위해 일시적으로 이용이 제한될 수 있음을 이해하고 동의합니다. 
+복구가 완료될 때까지 이용이 제한될 수 있습니다."
+                            ripple={false}
+                            // name="allAgreed"
+                            className="h-8 w-8 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                            crossOrigin={undefined}
+                            // checked={allAgreed}
+                            // onChange={handleAllAgreementChange}
+                        />
+                    </div>
+                    <Checkbox
+                        label="이상 위의 모든 조항을 충분히 이해하고, 이에 동의합니다."
+                        ripple={false}
+                        // name="allAgreed"
+                        className="h-8 w-8 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+                        crossOrigin={undefined}
+                        // checked={allAgreed}
+                        // onChange={handleAllAgreementChange}
+                    />
+                </div>
+
+                <div className="w-full flex flex-row justify-end items-center gap-3 text-white">
+                    <Button
+                        variant="outlined"
+                        className="rounded-lg border-[#F46221] text-[#F46221]  px-10 shadow-lg"
+                        // disabled={isConnecting}
+                        placeholder={undefined}>
+                        <span>취소</span>
+                    </Button>
+                    <Button
+                        className="rounded-lg px-10 bg-[#F46221] shadow-lg text-white"
+                        // disabled={isConnecting}
+                        placeholder={undefined}>
+                        <span>확인</span>
+                    </Button>
+                </div>
+            </div>
+
+            {/* <div className=" flex flex-row justify-start items-start w-full h-full">
                     <div className="w-[60%] h-full m-3">
                         <NftList
                             type="saza"
@@ -275,10 +373,10 @@ export default function ReportContainer(props: IReportContainerProps) {
                             </Button>
                         </div>
                     </div>
-                </div>
-                {/* <NftList type="saza" exNftList={sazaExNftList} setExNftList={setSazaExNftList} />
+                </div> */}
+            {/* <NftList type="saza" exNftList={sazaExNftList} setExNftList={setSazaExNftList} />
                 <NftList type="gaza" exNftList={gazaExNftList} setExNftList={setGazaExNftList} /> */}
-            </div>
+            {/* </div> */}
         </>
     )
 }
