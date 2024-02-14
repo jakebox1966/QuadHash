9e33e6d110597848441865c459396d7789bc2e18
Beomseok <jakebox1966@gmail.com>
2024-02-14 12:24:35 +0900

    no message

--- src/app/[locale]/common/layouts/header/SignInModal.tsx
+++ src/app/[locale]/common/layouts/header/SignInModal.tsx
@@ -55,6 +55,7 @@ export function SignInModal({ open, handleOpen }: ISignInModalProps) {
             //TODO 기존 프로세스는 회원가입 이력을 확인하는 로직이 들어가있지만, 새로운 로직에서는 필요가 없으므로 nonce값을 통해 처리하는 로직이 없어야한다.
 
             let result = await getUuidByAccount(accounts[0])
+            console.log('result', result)
 
             if (result.status === 'NotFound') {
                 const formData = new FormData()
@@ -67,6 +68,8 @@ export function SignInModal({ open, handleOpen }: ISignInModalProps) {
 
             const signature = await personalSign(accounts[0], result.eth_nonce)
 
+            console.log('signature', signature)
+
             const signInResult = await signIn('Credentials', {
                 wallet_address: accounts[0],
                 wallet_signature: signature,
@@ -74,7 +77,10 @@ export function SignInModal({ open, handleOpen }: ISignInModalProps) {
                 // redirect: false,
                 callbackUrl: callbackUrl,
             })
+
+            console.log('signInResult', signInResult)
         } catch (error) {
+            console.error(error)
             throw new Error('Error occured.')
         }
         setIsConnecting(false)
