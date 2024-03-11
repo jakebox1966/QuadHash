ea0df37befc0b13ecbf5412db2ba9429efe05335
Beomseok <jakebox1966@gmail.com>
2024-03-11 14:45:41 +0900

    #QuadHash
    -Qa반영

--- src/app/[locale]/(normal)/(page)/collectables/layout.tsx
+++ src/app/[locale]/(normal)/(page)/collectables/layout.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import MainWrapper from '../../../common/layouts/MainWrapper'
+import MainWrapperNoFooter from '@/app/[locale]/common/layouts/MainWrapperNoFooter'
 
 export interface IAboutLayoutProps {}
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-    return <MainWrapper>{children}</MainWrapper>
+    // return <MainWrapper>{children}</MainWrapper>
+
+    return <MainWrapperNoFooter>{children}</MainWrapperNoFooter>
 }
