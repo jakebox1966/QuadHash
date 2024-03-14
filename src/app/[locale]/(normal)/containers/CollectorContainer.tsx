beba710dbb86aa010533a690df9674f4112268b8
Beomseok <jakebox1966@gmail.com>
2024-03-15 05:38:47 +0900

    no message

--- src/app/[locale]/(normal)/containers/CollectorContainer.tsx
+++ src/app/[locale]/(normal)/containers/CollectorContainer.tsx
@@ -174,8 +174,10 @@ export default function CollectorContainer({
                     updateSession(token_id, token_type)
                 }
 
+                // 로그인 시 받아온 NFT tokenId가 Locked 인지 확인하고 Profile 이미지와 session 업데이트
                 if (isUsingLockedNFT) {
                     setProfileNFT('none')
+                    updateSession(token_id, token_type)
                 }
             } else {
                 setProfileNFT('none')
