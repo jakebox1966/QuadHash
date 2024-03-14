694163e365f3cc1b8541af4577c0b4aa0fd4370f
Beomseok <jakebox1966@gmail.com>
2024-03-15 06:01:47 +0900

    no message

--- src/app/[locale]/(normal)/components/main/MainFourthComponent.tsx
+++ src/app/[locale]/(normal)/components/main/MainFourthComponent.tsx
@@ -7,7 +7,7 @@ export interface IMainFourthComponentProps {}
 export default function MainFourthComponent(props: IMainFourthComponentProps) {
     return (
         <div className="w-full flex flex-row justify-center bg-[#FEFAE0]">
-            <div className="max-w-[1600px] pt-[80px] lg:pt-[566px]">
+            <div className="max-w-[1600px] pt-[60px] lg:pt-[453px]">
                 <Image src={main_fourth_image} alt="saza&gaza" />
             </div>
         </div>
